53b53b5cf6f4abd2f81f252347ab3c9a0daa784a
vargani4artem-boop <{message:Not Found,documentation_url:https://docs.github.com/rest/users/emails#list-email-addresses-for-the-authenticated-user,status:404}>
2026-03-28 17:40:47 -0600

    Optimize: Transition to Tailwind CSS v3 compiler for 100 PageSpeed

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         brand: {
           50: '#fff7ed',
           100: '#ffedd5',
+          400: '#fb923c',
           500: '#f97316',
           600: '#ea580c',
           900: '#7c2d12',
